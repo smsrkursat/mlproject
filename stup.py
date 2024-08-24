2a0a6f1533d6528f07bd9df4fb6ee3044582facb
smsrkursat <kursatsimsir8080@gmail.com>
2024-08-24 19:07:47 +0300

    setup.py and requirements.txt

---
+++ stup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+
+
+setup(
+name="mlproject",
+version="0.0.1",
+author="Kursat"
+author_email="kursatsimsir8080@gmail.com"
+packages=find_packages()
+install_requires=['pandas','numpy','seaborn'],
+
+)
